069939f4021664a1f77d3803c0c605aa49081d75
Kenneth Salicetti <ksalicetti0723@gmail.com>
2020-08-10 21:18:18 -0600

    Doc agregado

---
+++ docs/precache-manifest.dd4b5be58c4a4c9917410eb867adab4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03eb3c2e0212f42ae61d9428a571d7c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a07bf83643b16403c2ae",
+    "url": "/static/css/main.0bf3663a.chunk.css"
+  },
+  {
+    "revision": "9948515d998a14a1480d",
+    "url": "/static/js/2.6a91807f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a91807f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a07bf83643b16403c2ae",
+    "url": "/static/js/main.8f9f2729.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
